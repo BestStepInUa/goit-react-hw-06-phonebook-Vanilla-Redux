96c49cea51d3d64e354b5e873b446b94172abdfb
BestStepInUa <beststep.in.ua@gmail.com>
2024-01-30 14:30:32 +0200

    DONE

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import ContactItem from './ContactItem';
 import ContactsListSteled from './ContactsList.styled';
@@ -6,23 +5,27 @@ import ContactsListSteled from './ContactsList.styled';
 const ContactsList = () => {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
-  const normalizedFilter = filter.toLocaleLowerCase();
 
-  const visibleContactItems = useMemo(() => {
-    const visibleContacts = () => {
-      return contacts.filter(({ name }) =>
-        name.toLocaleLowerCase().includes(normalizedFilter)
-      );
-    };
-    visibleContacts.map(contact => (
-      <ContactItem key={contact.id} contact={contact} />
-    ));
-  }, [contacts, normalizedFilter]);
+  // const visibleContacts = useMemo(() => {
+  //   const normalizedFilter = filter.toLocaleLowerCase();
+  //   return contacts.filter(({ name }) =>
+  //     name.toLocaleLowerCase().includes(normalizedFilter)
+  //   );
+  // }, [contacts, filter]);
+
+  const normalizedFilter = filter.toLocaleLowerCase();
+  const visibleContacts = contacts.filter(({ name }) =>
+    name.toLocaleLowerCase().includes(normalizedFilter)
+  );
 
   return (
     <ContactsListSteled>
       <h2>Contacts</h2>
-      <ul>{visibleContactItems}</ul>
+      <ul>
+        {visibleContacts.map(contact => (
+          <ContactItem key={contact.id} contact={contact} />
+        ))}
+      </ul>
     </ContactsListSteled>
   );
 };
